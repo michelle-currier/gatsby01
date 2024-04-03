f2748673a3971041e9f8cba768f4bda7a2d11326
Michelle-Currier <michellecurrier@michelles-MacBook-Pro.local>
2024-04-03 13:12:33 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby01`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass"]
+};
