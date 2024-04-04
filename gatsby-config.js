40d18eb7ab18be142350e00cf42b87edd3b03d79
Michelle-Currier <michellecurrier@michelles-MacBook-Pro.local>
2024-04-04 14:11:24 -0500

    reformating

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,8 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby01`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: "Gatsby01",
+    siteUrl: "https://www.yourdomain.tld"
   },
   plugins: [
     "gatsby-plugin-sass",
@@ -15,7 +15,7 @@ module.exports = {
         short_name: "GatsbyJS",
         start_url: "/",
         background_color: "#6b37bf",
-        theme_color: "#6b37bf",
+        theme_color: "#6b37bf"
        // icon: "public/static/favicon.ico",
       }
     }
